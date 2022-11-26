ee7a79f5f4e692d419b65542b274cfc242acab56
Lautaro Nievas <lautynievas09@gmail.com>
2022-11-26 11:05:03 -0300

    characteres

--- TP4/js/lanzamiento.js
+++ TP4/js/lanzamiento.js
@@ -1,3 +1,22 @@
+let containerCharacters = document.querySelectorAll('.container-card-character');
+let characteres1 = document.querySelector('.zone-chars')
+let characteres2 = document.querySelector('.zone-chars2')
+
+console.log(characteres2);
+
+window.addEventListener('scroll', () =>{
+    if(window.scrollY >= 3400){
+        characteres1.classList.remove('zone-active');
+    }
+    if(window.scrollY >= 3912){
+        characteres2.classList.remove('zone-active');
+    }
+})
+
+
+
+
+
 const itemsGameplay = document.querySelector('.list-galery');
 const galeryNext = document.querySelector('.carousel-next');
 const galeryPrev = document.querySelector('.carousel-prev');
@@ -11,6 +30,7 @@ galeryNext.addEventListener('click', ()=>{
 	let lis = document.querySelectorAll(".slide-galery")
 	lis.forEach(x =>{
 		x.classList.remove('moveRightLi')
+        x.classList.remove('moveLeftLi')
 	})
 	setTimeout(() =>{
 		lis.forEach(x =>{
@@ -23,6 +43,7 @@ galeryPrev.addEventListener('click', ()=>{
 	let lis = document.querySelectorAll(".slide-galery")
 	lis.forEach(x =>{
 		x.classList.remove('moveLeftLi')
+        x.classList.remove('moveRightLi')
 	})
 	setTimeout(() =>{
 		lis.forEach(x =>{
@@ -30,58 +51,6 @@ galeryPrev.addEventListener('click', ()=>{
 		})
 	}, 100)
 })
-
-
-//CARROUSEL DE PERSONAJES
-const personajes = document.querySelectorAll('.card-character');
-const nextPersonajes = document.querySelector('.flecha-d-personajes');
-const prevPersonajes = document.querySelector('.flecha-i-personajes');
-let cont = 0;
-console.log(personajes)
-
-window.addEventListener('load', ()=>{
-	personajes[0].classList.add('active');
-	personajes[1].classList.add('active');
-})
-
-nextPersonajes.addEventListener('click', () =>{
-	paginationTwoPersonajes();
-})
-
-prevPersonajes.addEventListener('click',() =>{
-	paginationOnePersonajes();
-})
-const paginationOnePersonajes = () =>{
-    if(cont == 1){
-        personajes[2].classList.remove('active');
-        personajes[3].classList.remove('active');
-        personajes[0].classList.add('active');
-        personajes[1].classList.add('active');
-        cont--;
-    }else if(cont == 2){       
-        personajes[4].classList.remove('active');
-        personajes[5].classList.remove('active');
-        personajes[2].classList.add('active');
-        personajes[3].classList.add('active');
-        cont--;
-    }
-}
-
-const paginationTwoPersonajes = () =>{
-    if(cont == 0){
-        personajes[0].classList.remove('active');
-	    personajes[1].classList.remove('active');
-        personajes[2].classList.add('active');
-	    personajes[3].classList.add('active');
-        cont++;
-    }else if(cont == 1){
-        personajes[2].classList.remove('active');
-	    personajes[3].classList.remove('active');
-        personajes[4].classList.add('active');
-	    personajes[5].classList.add('active');
-        cont++;
-    }
-}
 
 //scroll history
 
@@ -92,9 +61,7 @@ let card1scroll = document.querySelector("#card-1-scroll");
 let card2scroll = document.querySelector("#card-2-scroll");
 let card3scroll = document.querySelector("#card-3-scroll");
 window.addEventListener('scroll', e =>{
-    console.log(window.scrollY);
     if(window.scrollY >= 1935 && window.scrollY < 2440){
-        console.log("object");
         card1scroll.classList.add('opacity-set');
         card2scroll.classList.remove('opacity-set');
         card3scroll.classList.add('opacity-set');
@@ -131,6 +98,5 @@ setInterval(()=>{
         sprite.classList.remove('step-'+contador);
         contador++;
         sprite.classList.add('step-'+contador);
-        console.log(contador);
     }
 },300)
